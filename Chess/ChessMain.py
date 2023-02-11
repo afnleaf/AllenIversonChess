534bd63690804b6f4fc666ae6a56de753ecaac89
afnleaf <afnleafow@gmail.com>
2023-02-11 00:41:42 -0500

    pieces move, prints moves to CLI

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -3,6 +3,8 @@ Main driver
 Handle user input and gamestate.
 """
 
+import sys
+import string
 import pygame as p
 import Engine
 
@@ -33,14 +35,47 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = Engine.GameState()
-    #print(gs.board)
     # do this once before the game while loop
     loadImages()
+
+    #draw on CLI first time
+    printBoard(gs.board)
+    
+    # keep track of last square clicked tuple(row, col)
+    squareSelected = ()
+    # track player clicks, two tuples 
+    playerClicks = []
     running = True
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+                print("Move log:")
+                print(gs.moveLog)
+            elif e.type == p.MOUSEBUTTONDOWN:
+                # get (x,y) location of the mouse
+                mousepos = p.mouse.get_pos()
+                row = mousepos[1]//SQ_SIZE
+                col = mousepos[0]//SQ_SIZE
+                # check if same square selected
+                if squareSelected == (row, col):
+                    squareSelected = ()
+                    playerClicks = []
+                else:
+                    squareSelected = (row, col)
+                    # append both 1st and 2nd click
+                    playerClicks.append(squareSelected)
+                # after second click
+                if len(playerClicks) == 2:
+                    # the engine makes the move
+                    move = Engine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    gs.makeMove(move)
+                    # draw to cli
+                    printBoard(gs.board)
+                    print(move.getChessNotation())
+                    # reset the user clicks
+                    squareSelected = ()
+                    playerClicks = []
 
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
@@ -49,6 +84,7 @@ def main():
 
 # Responsible for graphics of the current game state
 def drawGameState(screen, gs):
+    # draw graphical game state
     # draw squares
     drawBoard(screen)
     # add in piece highlights 
@@ -77,6 +113,24 @@ def drawPieces(screen, board):
             if piece != '--':
                 # what is blit()
                 screen.blit(IMAGES[piece], p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+# draw the pieces to the command line
+def printBoard(board):
+    n = len(board)
+    for i in range(n + 1):
+        if i < 8:
+            #print(str(i + ((i+1)*8)), end = ' ')
+            print(str(Engine.Move.rowsToRanks[i]), end = ' ')
+            for j in range(n):
+                print(board[i][j], end = ' ')
+            print()
+    # add letters at bottom
+    print("  ", end="")
+    for letter in string.ascii_uppercase:
+        print(letter + ' ', end = ' ')
+        if(letter == 'H'):
+            break
+    print()
 
 # default notation
 if __name__ == "__main__":
--- Chess/Engine.py
+++ Chess/Engine.py
@@ -27,5 +27,57 @@ class GameState():
 
         # track moves
         self.moveLog = []
+    
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = '--'
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        # log the move to undo it later
+        self.moveLog.append(move)
+        # switch turns
+        self.whiteToMove = not self.whiteToMove
+
+# for moves
+class Move():
+
+    # maps keys to values
+    # key : value
+    ranksToRows = { 
+        '1': 7,
+        '2': 6,
+        '3': 5,
+        '4': 4,
+        '5': 3,
+        '6': 2,
+        '7': 1,
+        '8': 0
+    }
+    rowsToRanks = {value: key for key, value in ranksToRows.items()}
+
+    filesToCols = {
+        'a': 0,
+        'b': 1,
+        'c': 2,
+        'd': 3,
+        'e': 4,
+        'f': 5,
+        'g': 6,
+        'h': 7
+    }
+    colsToFiles = {value: key for key, value in filesToCols.items()}
+
+    # start square, end square, board state
+    def __init__(self, start, end, board):
+        self.startRow = start[0]
+        self.startCol = start[1]
+        self.endRow = end[0]
+        self.endCol = end[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, row, col):
+        return self.colsToFiles[col] + self.rowsToRanks[row]
 
         
